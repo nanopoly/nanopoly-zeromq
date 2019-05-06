0246d612cbdb25b427b0199f1a39c6b6e81bc847
umu+ <aydinumut82@gmail.com>
2019-05-06 10:18:59 +0300

    basic test case implemented

---
+++ tests/transport.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const ZeroMQ = require('../index');
+
+const server = new ZeroMQ('rep');
+server.connect(3000, '127.0.0.1', 'bind');
+server.handle(() => server.send('pong'));
+
+const client = new ZeroMQ('req');
+client.connect(3000, '127.0.0.1', 'connect');
+
+describe('transport layer', () => {
+    afterAll(() => {
+        server.disconnect();
+        client.disconnect();
+    });
+
+    test('ping / pong', async (done) => {
+        client.handle(data => {
+            expect(data ? data.toString() : new Error('invalid data')).toBe('pong');
+            done();
+        });
+        client.send('ping');
+    });
+});
