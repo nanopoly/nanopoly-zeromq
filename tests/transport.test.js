dd80fb98ac84f01d39f734b636dab987c949cf35
umu+ <aydinumut82@gmail.com>
2019-09-04 11:50:27 +0300

    test case improved

--- tests/transport.test.js
+++ tests/transport.test.js
@@ -1,38 +1,37 @@
 'use strict';
 
-const ZeroMQ = require('../index');
+const { Client, Server } = require('../index');
 
-const client = new ZeroMQ({ log: 'fatal' });
-const server = new ZeroMQ({ server: true, log: 'fatal' });
+const client = new Client({ log: 'debug' });
+const server = new Server({ log: 'debug' });
 
 describe('zeromq transport layer', () => {
     let payload = Date.now();
 
-    beforeAll(async done => {
-        await server.start(async () => {
-            return payload;
-        });
+    beforeAll(done => {
+        server.start(async () => payload);
         setTimeout(done, 1000);
     });
 
-    afterAll(async done => {
+    afterAll(() => {
         client.stop();
         server.stop();
-        await server._registry.releasePort(server._options.port);
-        await client._registry.releasePort(client._options.port);
-        setTimeout(() => {
-            client._registry.stop();
-            server._registry.stop();
-            done();
-        }, 1000);
     });
 
     test('ping / pong', async done => {
-        await client.start(async r => {
+        client.start(async r => {
             expect(r.d).toBe(payload);
             done();
         });
-        await client.send({ i: '127.0.0.1', p: server._options.port },
-            { i: '127.0.0.1', p: client._options.port, d: payload });
+        setTimeout(() => {
+            let address;
+            for (let id in client._pair) address = client._pair[id][0];
+            client.send(address, { d: payload });
+        }, 1500);
+    });
+
+    test('invalid address', async done => {
+        expect(() => client.send('test', { d: payload })).toThrow();
+        done();
     });
 });
